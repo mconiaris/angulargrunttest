b3900a0952816aa1159f3a2843fc46872e24c554
mconiaris <mconiaris@gmail.com>
2016-01-21 10:50:27 -0500

    Copied template text into Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+ //Gruntfile
+    module.exports = function(grunt) {
+
+    //Initializing the configuration object
+      grunt.initConfig({
+
+        // Task configuration
+        concat: {
+          //...
+        },
+        less{
+          //...
+        },
+        uglify{
+          //...
+        },
+        watch{
+          //...
+        }
+      });
+
+    // Plugin loading
+
+    // Task definition
+
+  };
