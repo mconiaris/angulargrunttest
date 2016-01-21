124849a07a32bb4df13b8415f65c8fb195b880d0
mconiaris <mconiaris@gmail.com>
2016-01-21 10:54:29 -0500

    Copied tasks to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,25 @@
 
         // Task configuration
         concat: {
-          //...
+          options: {
+            separator: ';',
+          },
+          js_frontend: {
+            src: [
+              './bower_components/jquery/jquery.js',
+              './bower_components/bootstrap/dist/js/bootstrap.js',
+              './app/assets/javascript/frontend.js'
+            ],
+            dest: './public/assets/javascript/frontend.js',
+          },
+          js_backend: {
+            src: [
+              './bower_components/jquery/jquery.js',
+              './bower_components/bootstrap/dist/js/bootstrap.js',
+              './app/assets/javascript/backend.js'
+            ],
+            dest: './public/assets/javascript/backend.js',
+          },
         },
         less{
           //...
