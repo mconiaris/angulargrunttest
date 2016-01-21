41331dc1d0efcfa16018f223cd21cfa03942dc21
mconiaris <mconiaris@gmail.com>
2016-01-21 11:11:02 -0500

    Copied over changes to Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,83 @@
- //Gruntfile
-    module.exports = function(grunt) {
+module.exports = function(grunt) {
 
-    //Initializing the configuration object
-      grunt.initConfig({
+  //Initializing the configuration object
+    grunt.initConfig({
 
-        // Task configuration
-        concat: {
-          options: {
-            separator: ';',
-          },
-          js_frontend: {
-            src: [
-              './bower_components/jquery/jquery.js',
-              './bower_components/bootstrap/dist/js/bootstrap.js',
-              './app/assets/javascript/frontend.js'
-            ],
-            dest: './public/assets/javascript/frontend.js',
-          },
-          js_backend: {
-            src: [
-              './bower_components/jquery/jquery.js',
-              './bower_components/bootstrap/dist/js/bootstrap.js',
-              './app/assets/javascript/backend.js'
-            ],
-            dest: './public/assets/javascript/backend.js',
-          },
-        },
-        less{
-          //...
-        },
-        uglify{
-          //...
-        },
-        watch{
-          //...
+      // Task configuration
+    concat: {
+      options: {
+        separator: ';',
+      },
+      js_frontend: {
+        src: [
+          './bower_components/jquery/jquery.js',
+          './bower_components/bootstrap/dist/js/bootstrap.js',
+          './app/assets/javascript/frontend.js'
+        ],
+        dest: './public/assets/javascript/frontend.js',
+      },
+      js_backend: {
+        src: [
+          './bower_components/jquery/jquery.js',
+          './bower_components/bootstrap/dist/js/bootstrap.js',
+          './app/assets/javascript/backend.js'
+        ],
+        dest: './public/assets/javascript/backend.js',
+      },
+    },
+    uglify: {
+      options: {
+        mangle: false  // Use if you want the names of your functions and variables unchanged
+      },
+      frontend: {
+        files: {
+          './public/assets/javascript/frontend.js': './public/assets/javascript/frontend.js',
         }
-      });
+      },
+      backend: {
+        files: {
+          './public/assets/javascript/backend.js': './public/assets/javascript/backend.js',
+        }
+      },
+    },
+    watch: {
+        js_frontend: {
+          files: [
+            //watched files
+            './bower_components/jquery/jquery.js',
+            './bower_components/bootstrap/dist/js/bootstrap.js',
+            './app/assets/javascript/frontend.js'
+            ],
+          tasks: ['concat:js_frontend','uglify:frontend'],     //tasks to run
+          options: {
+            livereload: true                        //reloads the browser
+          }
+        },
+        js_backend: {
+          files: [
+            //watched files
+            './bower_components/jquery/jquery.js',
+            './bower_components/bootstrap/dist/js/bootstrap.js',
+            './app/assets/javascript/backend.js'
+          ],
+          tasks: ['concat:js_backend','uglify:backend'],     //tasks to run
+          options: {
+            livereload: true                        //reloads the browser
+          }
+        },
+        tests: {
+          files: ['app/controllers/*.php','app/models/*.php'],  //the task will run only when you save files in this location
+        }
+      }
+    });
 
-    // Plugin loading
+  // Plugin loading
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    // Task definition
+  // Task definition
+  grunt.registerTask('default', ['watch']);
 
-  };
+};
